7628e1be54d36a1033f4fcbd3559465df5ba56e6
Karstenheiseldal <karstenheiseldal@outlook.com>
2023-10-04 13:30:03 +0200

    title akas changes
    title akas changes after talk with supervisor

--- database/B2_build_movie_db.sql
+++ database/B2_build_movie_db.sql
@@ -19,92 +19,18 @@ drop table if exists TitleAkas;
 CREATE TABLE TitleAkas(
 	titleID varchar(255) NOT NULL,
 	titleName varchar(255) not null,
+    region varchar(255),
+    attribute varchar(255),
+    typeName varchar (255),
+    LanguageName varchar(255),
+    ordering int,
 	isOriginalTitle boolean,
 	poster varchar(255),
 	plot varchar(255),
-	primary key (titleID, titleName),
+	primary key (titleID, titleName, region),
 	foreign key (titleID) references titlebasics(titleID)
 );
 
-drop table if exists Region;
-Create table Region (
-    regionID serial not null,
-    regionName varchar(255) not null,
-    PRIMARY KEY (regionID)
-);
-
-drop table if exists RegionAssociation;
-Create table RegionAssociation (
-    titleID varchar(255) not null,
-	regionID serial not null,
-    PRIMARY KEY (titleID, regionID),
-	FOREIGN KEY (titleID) REFERENCES TitleBasics(titleID),
-    FOREIGN KEY (regionID) REFERENCES Region(regionID)
-);
-
-drop table if exists Type;
-Create table Type (
-    typeID serial not null,
-    typeName varchar(255) not null,
-    PRIMARY KEY (typeID)
-);
-
-drop table if exists TypeAssociation;
-Create table TypeAssociation (
-    titleID varchar(255) not null,
-    typeID serial not null,
-    PRIMARY KEY (titleID, typeID),
-	FOREIGN KEY (titleID) REFERENCES TitleBasics(titleID),
-    FOREIGN KEY (typeID) REFERENCES Type(typeID)
-);
-
-drop table if exists Language;
-Create table Language (
-    langID SERIAL not null,
-    langName varchar(255) not null,
-    PRIMARY KEY (langID)
-);
-
-drop table if exists LanguageAssociation;
-Create table LanguageAssociation (
-    titleID varchar(255) not null,
-    langID serial not null,
-    PRIMARY KEY (titleID, langID),
-	FOREIGN KEY (titleID) REFERENCES TitleBasics(titleID),
-    FOREIGN KEY (langID) REFERENCES Language(langID)
-);
-
-drop table if exists Attribute;
-Create table Attribute (
-    attID SERIAL not null,
-    attName varchar(255) not null,
-    PRIMARY KEY (attID)
-);
-
-drop table if exists AttributeAssociation;
-Create table AttributeAssociation (
-    titleID varchar(255) not null,
-    attID SERIAL not null,
-    PRIMARY KEY (titleID, attID),
-	FOREIGN KEY (titleID) REFERENCES TitleBasics(titleID),
-    FOREIGN KEY (attID) REFERENCES Attribute(attID)
-);
-
-drop table if exists Ordering;
-Create table Ordering (
-    orderID SERIAL not null,
-    orderNumber int not null,
-    PRIMARY KEY (orderID)
-);
-
-drop table if exists OrderingAssociation;
-Create table OrderingAssociation (
-    titleID varchar(255) not null,
-    orderID int not null,
-    PRIMARY KEY (titleID, orderID),
-	FOREIGN KEY (titleID) REFERENCES TitleBasics(titleID),
-    FOREIGN KEY (orderID) REFERENCES Ordering(orderID)
-);
 
 drop table if exists Episode;
 Create table Episode (
