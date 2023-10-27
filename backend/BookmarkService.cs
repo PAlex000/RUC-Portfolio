095db12d2236c69fe7dd1259e1d8e9a3b0690809
Pozsgai Alex <jatekalex@gmail.com>
2023-10-27 14:36:41 +0200

    Start bookmarkService and add MovieContex

---
+++ backend/BookmarkService.cs
@@ -0,0 +1,18 @@
+﻿using backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend
+{
+    public class BookmarkService : IBookmarkService
+    {
+        private readonly MovieContext db = new MovieContext();
+        public IList<Bookmark> GetBookmarks()
+        {
+            return new List<Bookmark>();
+        }
+    }
+}
