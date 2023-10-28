cf1f8635c42f492965b95dad7ca2f22d35b0cb33
surrenderat20 <lassevfuglsbjerg@gmail.com>
2023-10-28 14:26:11 +0200

    Models for movies

---
+++ backend/Models/TitleBasics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    internal class TitleBasics
+    {
+        public string titleID { get; set; }
+        public string type { get; set; }
+        public bool isAdult { get; set; }
+        public string startYear { get; set; }
+        public string endYear { get; set; }
+        public string poster {  get; set; }
+        public string description { get; set; }
+        public int rating { get; set; }
+
+    }
+}
