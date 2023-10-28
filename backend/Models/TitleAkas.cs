2dcb921ec315fdfa804a5cc2a38faf697cc2058f
surrenderat20 <lassevfuglsbjerg@gmail.com>
2023-10-28 14:26:11 +0200

    Models for movies

---
+++ backend/Models/TitleAkas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    internal class TitleAkas
+    {
+        public string? titleID {  get; set; }
+        public int ordering {  get; set; }
+        public string? title { get; set; }
+        public string? region { get; set; }
+        public string? attribute { get; set; }
+        public string? typeName { get; set; }
+        public string? languagename { get; set; }
+        public bool isOriginalTitle { get; set; }
+
+    }
+}
