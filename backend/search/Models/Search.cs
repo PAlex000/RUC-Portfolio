22e92f87bb95bd3769b727fcbbb641e4a13abec1
Pozsgai Alex <jatekalex@gmail.com>
2023-10-30 15:09:47 +0100

    Add tests for SearchHistory

--- backend/search/Models/Search.cs
+++ backend/search/Models/Search.cs
@@ -1,9 +1,10 @@
 ﻿namespace search.Models
 {
+    //TODO: DateTime.now cannot be converted to Datetime for some reason
     public class Search
     {
         public int userID { get; set; }
-        public string searchString { get; set; }
+        public string? searchString { get; set; }
         public DateTime searchDate { get; set; }
     }
 }
