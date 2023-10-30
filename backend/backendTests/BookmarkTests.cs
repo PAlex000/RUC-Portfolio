048dcdb26ec0308c592d008ef182d4128e42b6d3
Pozsgai Alex <jatekalex@gmail.com>
2023-10-30 13:21:03 +0100

    Implement deleteBookmark, createBookmark

--- backend/backendTests/BookmarkTests.cs
+++ backend/backendTests/BookmarkTests.cs
@@ -28,5 +28,22 @@ namespace backendTests
             var bookmark = service.GetBookmarkById(1);
             Assert.Equal("tt10577876", bookmark?.titleID);
         }
+        [Fact]
+        public void DeleteBookmark_ValidId_RemoveBookmark()
+        {
+            var service = new BookmarkService();
+            var bookmark = service.CreateBookmark(1, "tt10598994");
+            var result = service.DeleteBookmark(bookmark.ID);
+            Assert.True(result);
+            bookmark = service.GetBookmarkById(bookmark.ID);
+            Assert.Null(bookmark);
+        }
+        [Fact]
+        public void DeleteBookmark_InvalidId_ReturnsFalse()
+        {
+            var service = new BookmarkService();
+            var result = service.DeleteBookmark(-1);
+            Assert.False(result);
+        }
     }
 }
--- backend/bookmark/BookmarkService.cs
+++ backend/bookmark/BookmarkService.cs
@@ -18,6 +18,33 @@ namespace bookmark
         {
             return db.Bookmarks.FirstOrDefault(x => x.ID == bookmarkId);
         }
-
+        public Bookmark? CreateBookmark(int userId, string titleId)
+        {
+            var id = db.Bookmarks.Max(x => x.ID) + 1;
+            var bookmark = new Bookmark
+            {
+                ID = id,
+                titleID = titleId,
+                userID = userId,
+                status = true
+            };
+            db.Add(bookmark);
+            db.SaveChanges();
+            return bookmark;
+        }
+        public bool DeleteBookmark(Bookmark bookmark)
+        {
+            return DeleteBookmark(bookmark.ID);
+        }
+        public bool DeleteBookmark(int bookmarkId)
+        {
+            var bookmark = db.Bookmarks.FirstOrDefault(x => x.ID == bookmarkId);
+            if (bookmark != null)
+            {
+                db.Bookmarks.Remove(bookmark);
+                return db.SaveChanges() > 0;
+            }
+            return false;
+        }
     }
 }
