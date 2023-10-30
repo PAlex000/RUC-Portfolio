ffedb6b4e64d133b54e6fc2e134d4babe22bc80a
surrenderat20 <lassevfuglsbjerg@gmail.com>
2023-10-30 23:20:56 +0100

    interface and crud ops

---
+++ backend/IMovieService.cs
@@ -0,0 +1,14 @@
+﻿using backend.Models;
+using System.Collections.Generic;
+
+namespace backend
+{
+    public interface IMovieService
+    {
+        List<TitleBasics> SearchMovies(int userId, string searchString);
+        List<TitleBasics> GetSimilarMovies(string movieId);
+        bool CreateMovie(TitleBasics newMovie, TitleAkas newTitleAkas);
+        bool DeleteMovie(string movieId);
+        void UpdateMovie(string movieId, TitleBasics updatedMovie, TitleAkas updatedTitleAkas);
+    }
+}
