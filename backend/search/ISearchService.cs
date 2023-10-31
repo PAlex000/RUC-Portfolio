859580f22f813f0cff0a956fdccf4e3f10e3223f
Pozsgai Alex <jatekalex@gmail.com>
2023-10-31 11:23:41 +0100

    Comment out a test for deleteSearch because it's buggy

--- backend/search/ISearchService.cs
+++ backend/search/ISearchService.cs
@@ -7,7 +7,7 @@ namespace search
         public IList<Search> GetSearchHistory();
         public IList<Search> GetSearchHistoryByUserId(int _userID);
         public Search? CreateSearch(int userId, string searchString);
-        public bool DeleteSearch(Search searchHistory);
+        //public bool DeleteSearch(Search searchHistory);
         public bool DeleteSearch(string searchString, int _userID);
     }
 }
