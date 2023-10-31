8fc0ff3912a7eba1f72f7f3e9a7ccffe97c84735
Pozsgai Alex <jatekalex@gmail.com>
2023-10-31 13:15:44 +0100

    Implement CRUD for user

---
+++ backend/backend/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using backend.Models;
+
+namespace backend
+{
+    public interface IUserService
+    {
+        public IList<User> GetUsers();
+        public User GetUserById(int _userId);
+        public User GetUserByEmail(string _email);
+        public User CreateUser(string _firstName, string _lastName, string _email, string _pwdHash, string? _phoneNo);
+        public bool UpdateUserEmail(int _userid, string _email);
+        public bool UpdateUserPassword(int _userid, string _pwdHash);
+        public bool DeleteUser(User user);
+        public bool DeleteUser(int _userid);
+    }
+}
