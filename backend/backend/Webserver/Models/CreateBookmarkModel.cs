14c0b3cd9c83c8e1cfbc3c8abf06752c84656f82
cristinaBodnari <cristina.bodnari12@gmail.com>
2023-11-08 15:21:51 +0100

    Update

--- backend/backend/Webserver/Models/CreateBookmarkModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebServer.Models;
-
-public class CreateBookmarkModel
-{
-    public string? titleID { get; set; }
-    public int userID { get; set; }
-    public bool status { get; set; }
-
-}
--- database/Indexing.sql
+++
@@ -1,5 +0,0 @@
-
-CREATE INDEX idx_primaryName ON person(primaryname);
-CREATE INDEX idx_job ON personassociation(job);
-CREATE INDEX idx_titleName ON titleakas(titlename);
-CREATE INDEX idx_word ON wi(word);
