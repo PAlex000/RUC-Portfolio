0589e1c9ddb53233bfbdb54a4d51a28e5f64dc54
Pozsgai Alex <jatekalex@gmail.com>
2023-11-20 10:46:03 +0100

    Readd database which was deleted by accident

---
+++ database/B2_build_movie_db.sql
@@ -0,0 +1,74 @@
+drop table if exists Genre;
+create table Genre(
+	genreID SERIAL NOT NULL,
+	genreName varchar(100) NOT NULL,
+	primary key (genreID)
+);
+
+drop table if exists TitleBasics;
+Create table TitleBasics(
+	titleID varchar(255) NOT NULL,
+	titleType varchar(100) NOT NULL,
+	isAdult boolean NOT NULL,
+	startYear varchar(255) NOT NULL,
+	endYear varchar(255) NOT NULL,
+	poster varchar(255),
+    plot text,
+	movie_rating int,
+	primary key (titleID)
+);
+
+drop table if exists TitleAkas;
+CREATE TABLE TitleAkas(
+	titleID varchar(255) NOT NULL,
+    ordering SERIAL not null,
+	titleName varchar(255),
+    region varchar(255),
+    attribute varchar(255),
+    typeName varchar (255),
+    LanguageName varchar(255),
+	isOriginalTitle boolean,
+	primary key (titleID, ordering),
+	foreign key (titleID) references titlebasics(titleID)
+);
+
+
+drop table if exists Episode;
+Create table Episode (
+    titleID varchar(255) not null,
+    episodeTitleID varchar(255) not null,
+	seasonNumber int,
+	episodeNumber int,
+    PRIMARY KEY (titleID, episodeTitleID),
+	FOREIGN KEY (titleID) REFERENCES TitleBasics(titleID)
+);
+
+drop table if exists GenreAssociation;
+create table GenreAssociation(
+	genreID SERIAL NOT NULL,
+    titleID varchar(255) NOT NULL,
+    primary key (titleID, genreID),
+    FOREIGN KEY (genreID) REFERENCES Genre(genreID),
+    FOREIGN KEY (titleID) REFERENCES TitleBasics(titleID)
+);
+
+drop table if exists Person;
+Create table Person(
+	personID varchar(255) NOT NULL,
+	primaryname varchar(255),
+	dateOfBirth varchar(255),
+	dateOfDeath varchar(255),
+	name_rating Float,
+	primary key (personID)
+);
+
+drop table if exists PersonAssociation;
+Create table PersonAssociation(
+	titleID varchar(255) NOT NULL,
+	personID varchar(255) NOT NULL,
+	ordering int not null,
+	job varchar(255) NOT NULL,
+	primary key (titleID, personID, ordering),
+	FOREIGN KEY (titleID) REFERENCES TitleBasics(titleID),
+    FOREIGN KEY (personID) REFERENCES Person(personID)
+);
---
+++ database/C2_build_framework_db.sql
@@ -0,0 +1,42 @@
+drop table if exists UserRelation;
+Create table UserRelation(
+	userID SERIAL NOT NULL,
+	firstName VARCHAR(40) NOT NULL,
+	lastName VARCHAR(40) NOT NULL,
+	email VARCHAR(150) NOT NULL,
+	pwdHash VARCHAR(255) NOT NULL,
+	phoneNo VARCHAR(20),
+	isVerified BOOLEAN NOT NULL,
+	isActive BOOLEAN NOT NULL,
+	PRIMARY KEY (userID)
+);
+
+Create table Bookmarks(
+	bookmarkID SERIAL NOT NULL,
+	userID SERIAL NOT NULL,
+	titleID varchar(255) NOT NULL,
+	status boolean not null,
+	PRIMARY KEY(bookmarkID, userID, titleID),
+	FOREIGN KEY(titleID) REFERENCES TitleBasics(titleID),
+	foreign key(userID) references UserRelation(userID)
+);
+
+drop table if exists Rating;
+Create table Rating (
+    titleID varchar(255) not null,
+    userID SERIAL not null,
+    grade INT not null,
+    reviewText TEXT not null,
+    rateDate varchar(255) not null,
+    PRIMARY KEY (titleID, userID),
+    FOREIGN KEY (titleID) REFERENCES TitleBasics(titleID),
+	FOREIGN KEY (userID) REFERENCES UserRelation(userID)
+);
+
+CREATE table Search (
+	userID SERIAL NOT NULL,
+	searchString VARCHAR(255),
+	searchDate date,
+	PRIMARY KEY (userID, searchString),
+	FOREIGN KEY (userID) REFERENCES UserRelation (userID)
+);
