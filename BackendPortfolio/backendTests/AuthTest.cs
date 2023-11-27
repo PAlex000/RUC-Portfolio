65651bd8450d48ff20253294b73d19f97aeeddfb
Karstenheiseldal <karstenheiseldal@outlook.com>
2023-11-27 19:18:55 +0100

    Auth service for generering av jwt

---
+++ BackendPortfolio/backendTests/AuthTest.cs
@@ -0,0 +1,36 @@
+﻿using DataLayer.Database;
+using DataLayer.Models;
+using Newtonsoft.Json;
+using System.Text;
+using WebServer.Controllers;
+using WebServer.Models;
+using Xunit;
+
+namespace BackendTests
+{
+    public class AuthTests
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthTests()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:5001")
+            };
+        }
+        [Fact]
+        public async Task Login_ValidCredentials_ReturnsToken()
+        {
+            var loginRequest = new UserModel { email = "lasse.doe@example.com", password = "hashed_password1" };
+            var content = new StringContent(JsonConvert.SerializeObject(loginRequest), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("/api/user/login", content);
+
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var token = JsonConvert.DeserializeAnonymousType(responseContent, new { Token = "" });
+            Assert.NotNull(token.Token);
+        }
+    }
+}
--- BackendPortfolio/backendTests/UserTests.cs
+++ BackendPortfolio/backendTests/UserTests.cs
@@ -13,25 +13,11 @@ namespace BackendTests
         private readonly HttpClient _httpClient;
 
         public UserTests()
-        { 
+        {
             _httpClient = new HttpClient
             {
                 BaseAddress = new Uri("http://localhost:5001")
             };
-        }
-
-        [Fact]
-        public async Task Login_ValidCredentials_ReturnsToken()
-        {
-            var loginRequest = new UserModel { email = "lasse.doe@example.com", password = "hashed_password1" };
-            var content = new StringContent(JsonConvert.SerializeObject(loginRequest), Encoding.UTF8, "application/json");
-
-            var response = await _httpClient.PostAsync("/api/user/login", content);
-
-            response.EnsureSuccessStatusCode();
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var token = JsonConvert.DeserializeAnonymousType(responseContent, new { Token = "" });
-            Assert.NotNull(token.Token);
         }
 
         [Fact]
