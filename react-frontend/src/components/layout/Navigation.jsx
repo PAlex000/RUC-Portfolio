763db6f109004bd348462110bd7a809bad21a07c
surrenderat20 <lvf@silvi.ai>
2023-12-08 16:16:40 +0100

    design update navBar 2 | now toggle functionality

--- react-frontend/src/components/layout/Navigation.jsx
+++ react-frontend/src/components/layout/Navigation.jsx
@@ -4,53 +4,47 @@ import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import { Link } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
+import { List } from "react-bootstrap-icons";
 import "./Navigation.scss";
 
 const Navigation = () => {
   return (
     <Navbar className="navbar-custom" bg="dark" expand="lg">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <FaBars />
-        </Navbar.Toggle>
-
+      <div className="burger-icon-container">
+        <List className="burger-icon" style={{ color: "#fff" }} />
+      </div>
+      <Container className="justify-content-start">
         <Navbar.Brand as={Link} to="/" className="logo">
-          <Link to="/" className="nav-link">
-            <div className="text-black">IMDB</div>
-          </Link>
+          IMDB
         </Navbar.Brand>
-
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Form className="search-wrapper d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search Recommendations..."
-              className="me-2"
-              aria-label="Search"
-            />
-          </Form>
-          <Nav className="me-auto">
-            <Link to="/explorer" className="nav-link">
-              Explorer
-            </Link>
-            <Link to="/details" className="nav-link">
-              Details
-            </Link>
-            <Link to="/bookmark" className="nav-link">
-              Bookmark
-            </Link>
-            <Link to="/ratinghistory" className="nav-link">
-              RatingHistory
-            </Link>
-            <Link to="/bookmarks" className="nav-link">
-              Watchlist
-            </Link>
-            <Link to="/login" className="nav-link">
-              Sign in
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Form className="search-wrapper d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search Recommendations..."
+            className="me-2"
+            aria-label="Search"
+          />
+        </Form>
+        <Nav className="ms-auto">
+          <Link to="/explorer" className="nav-link">
+            Explorer
+          </Link>
+          <Link to="/details" className="nav-link">
+            Details
+          </Link>
+          <Link to="/bookmark" className="nav-link">
+            Bookmark
+          </Link>
+          <Link to="/ratinghistory" className="nav-link">
+            RatingHistory
+          </Link>
+          <Link to="/bookmarks" className="nav-link">
+            Watchlist
+          </Link>
+          <Link to="/login" className="nav-link">
+            Sign in
+          </Link>
+        </Nav>
       </Container>
     </Navbar>
   );
