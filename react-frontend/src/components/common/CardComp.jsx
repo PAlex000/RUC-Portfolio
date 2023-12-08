4258461f24dc8a0b3e7a1d27cde6ba8a7167b24d
surrenderat20 <lvf@silvi.ai>
2023-12-08 12:55:45 +0100

    trailer, information

--- react-frontend/src/components/common/CardComp.jsx
+++ react-frontend/src/components/common/CardComp.jsx
@@ -43,15 +43,17 @@ const CardComp = ({ title, rating, image, onClick }) => {
         <div className="d-flex align-items-center">
           <StarFill color="yellow" className="mb-2" />
           <p className="text-white mb-2 ms-2">{rating}</p>
-          <Star className="text-primary mb-2 ms-4" />
+          <Star className="text-primary mb-2 ms-4" />{" "}
+          {/* OnCLick prop in star to add to favorites*/}
         </div>
         <Card.Title className="text-white" style={titleStyle}>
           {title}
         </Card.Title>
         <WatchlistButton>
-          <p className="text-primary" style={textStyle}>
+          <div className="d-flex justify-content-center align-items-center text-primary">
+            <p className="text-primary"></p>
             <Plus style={{ fontSize: "24px" }} /> Watchlist
-          </p>
+          </div>
         </WatchlistButton>
         <Container>
           <Row style={{ color: "#fff", marginTop: "1rem" }}>
