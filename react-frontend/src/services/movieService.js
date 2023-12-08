ea9f838800f38c97a9e814e79bedeb6389abc337
surrenderat20 <lvf@silvi.ai>
2023-12-08 20:56:33 +0100

    a whole lot of types, actions and thunk actions for movies | redux

--- react-frontend/src/services/movieService.js
+++ react-frontend/src/services/movieService.js
@@ -1,4 +1,3 @@
-//potentially not needed anymore as the thunk actions now seems responsible for the api calls.
 export const getMovies = async (endpoint) => {
   const response = await fetch(endpoint);
   const data = await response.json();
