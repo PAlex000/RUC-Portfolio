7063b599a6b197ab46c7fa5a780ce946cec1f849
surrenderat20 <lvf@silvi.ai>
2023-12-08 15:56:37 +0100

    navBar design update

--- react-frontend/src/components/layout/Navigation.jsx
+++ react-frontend/src/components/layout/Navigation.jsx
@@ -4,40 +4,53 @@ import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 import "./Navigation.scss";
 
 const Navigation = () => {
   return (
     <Navbar className="navbar-custom" bg="dark" expand="lg">
       <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <FaBars />
+        </Navbar.Toggle>
+
         <Navbar.Brand as={Link} to="/" className="logo">
-          IMDB
-        </Navbar.Brand>
-        <Form className="search-wrapper d-flex">
-          <FormControl
-            type="search"
-            placeholder="Search Recommendations..."
-            className="me-2"
-            aria-label="Search"
-          />
-        </Form>
-        <Nav className="me-auto">
           <Link to="/" className="nav-link">
-            Home
+            <div className="text-black">IMDB</div>
           </Link>
-          <Link to="/explorer" className="nav-link">
-            Explorer
-          </Link>
-          <Link to="/details" className="nav-link">
-            Details
-          </Link>
-          <Link to="/bookmark" className="nav-link">
-            Bookmark
-          </Link>
-          <Link to="/ratinghistory" className="nav-link">
-            RatingHistory
-          </Link>
-        </Nav>
+        </Navbar.Brand>
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Form className="search-wrapper d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search Recommendations..."
+              className="me-2"
+              aria-label="Search"
+            />
+          </Form>
+          <Nav className="me-auto">
+            <Link to="/explorer" className="nav-link">
+              Explorer
+            </Link>
+            <Link to="/details" className="nav-link">
+              Details
+            </Link>
+            <Link to="/bookmark" className="nav-link">
+              Bookmark
+            </Link>
+            <Link to="/ratinghistory" className="nav-link">
+              RatingHistory
+            </Link>
+            <Link to="/bookmarks" className="nav-link">
+              Watchlist
+            </Link>
+            <Link to="/login" className="nav-link">
+              Sign in
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
