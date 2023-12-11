249acb940f4edb4efa4a2665941b58eea629f964
Filoftea-Bianca <myangeldankouzo00@gmail.com>
2023-12-11 21:43:56 +0100

    added modals

--- react-frontend/src/components/common/CardSearch.jsx
+++ react-frontend/src/components/common/CardSearch.jsx
@@ -6,62 +6,70 @@ import {
     // StarFill,
     InfoCircleFill,
   } from "react-bootstrap-icons";
+import {useState} from "react"
+import Modal from "react-bootstrap/Modal";
 
+const cardStyle = {
+  backgroundColor: "#313131",
+  color: "white",
+  width: "14rem",
+  height: "27rem",
+};
+
+const titleStyle = {
+  display: "-webkit-box",
+  WebkitBoxOrient: "vertical",
+  WebkitLineClamp: 2,
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  lineHeight: "1.2em",
+  maxHeight: "2.4em",
+};
+
+const imageStyle = {
+  height: "220px",
+  objectFit: "cover",
+};
+
+const cardFooterStyle = {
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  width: "100%",
+  borderTop: "1px solid rgba(0,0,0,.125)",
+  padding: "0.75rem 1.25rem",
+  backgroundColor: "#313131",
+};
+
+const buttonStyle = {
+  fontWeight: "bold"
+}
+
+const cardFooterStyleSecond = {
+  position: "absolute",
+  bottom: -40,
+  left: 0,
+  width: "100%",
+  height: "auto",
+  borderTop: "1px solid rgba(0,0,0,.125)",
+  padding: "0.40rem 0.85rem",
+  backgroundColor: "#676767",
+};
+
+const linkStyle = {
+  color: "#FFFFFF", 
+  fontWeight: "bold", 
+  fontSize: "15px", 
+  textDecoration: "none",
+  paddingBottom: "0.5rem"
+}
 
 const CardSearch = ({ title, image }) => {
-  const cardStyle = {
-    backgroundColor: "#313131",
-    color: "white",
-    width: "14rem",
-    height: "28rem",
-  };
 
-  const titleStyle = {
-    display: "-webkit-box",
-    WebkitBoxOrient: "vertical",
-    WebkitLineClamp: 2,
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    lineHeight: "1.2em",
-    maxHeight: "2.4em",
-  };
+  const [show, setShow] = useState(false);
 
-  const imageStyle = {
-    height: "220px",
-    objectFit: "cover",
-  };
-
-  const cardFooterStyle = {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    width: "100%",
-    borderTop: "1px solid rgba(0,0,0,.125)",
-    padding: "0.75rem 1.25rem",
-    backgroundColor: "#313131",
-  };
-
-  const buttonStyle = {
-    fontWeight: "bold"
-  }
-
-  const cardFooterStyleSecond = {
-    position: "absolute",
-    bottom: -45,
-    left: 0,
-    width: "100%",
-    height: "auto",
-    borderTop: "1px solid rgba(0,0,0,.125)",
-    padding: "0.75rem 1rem",
-    backgroundColor: "#676767",
-  };
-
-  const linkStyle = {
-    color: "#FFFFFF", 
-    fontWeight: "bold", 
-    fontSize: "15px", 
-    textDecoration: "none"
-  }
+  const handleClose = () => setShow(false); // = () => , not just equal, no wonder why it was re-rendering all over again
+  const handleShow = () => setShow(true);
 
 
   return (
@@ -78,17 +86,39 @@ const CardSearch = ({ title, image }) => {
         </ListGroup>
         <div style={cardFooterStyle}>
           <div className="d-flex justify-content-between flex-column align-items-center mt-2">
-            <Button variant="warning" className="mb-3" style={buttonStyle}>Add as favorite <FaRegHeart style={{fontSize:"18px"}} className="mb-1"/></Button>
-            <Button variant="danger" className="mb-4" style={buttonStyle}>Delete search</Button>
+            <Button variant="warning" className="mb-3" style={buttonStyle} >Add as favorite <FaRegHeart style={{fontSize:"18px"}} className="mb-1"/></Button>
+            <Button variant="danger" className="mb-4" style={buttonStyle} onClick={handleShow}>Delete search</Button>
+
+            <Modal show={show} onHide={handleClose} backdrop="static"
+        keyboard={false} style={{
+          fontSize: "20px",
+          fontWeight: "bold",
+        }}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete search</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this record? You cannot undo this action.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose} style={{fontWeight: "bold", fontSize: "20px"}}>
+            Yes
+          </Button>
+          <Button variant="warning" onClick={handleClose} style={{fontWeight: "bold", fontSize: "20px"}}>
+            No, take me back!
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
           </div>
         </div>
         <div style={cardFooterStyleSecond}>
             <div className="d-flex justify-content-between align-items-center mt-2">
-            <a href="#details" style={linkStyle}>View movie description</a><InfoCircleFill color="white" size="18px"/>
+            <a href="#details" style={linkStyle}>View movie description</a><InfoCircleFill color="white" size="18px" style={{marginBottom: "0.5rem"}}/>
         </div>
         </div>
       </Card.Body>
     </Card>
+
+         
   );
 };
 
--- react-frontend/src/components/privateViews/ProfileSettings.jsx
+++ react-frontend/src/components/privateViews/ProfileSettings.jsx
@@ -11,11 +11,13 @@ import { MdOutlineEmail } from "react-icons/md";
 import { MdOutlinePermPhoneMsg } from "react-icons/md";
 import shawshank from "../../assets/shawshank.jpg";
 import departed from "../../assets/departed.jpg";
-import CardComp from "../common/CardComp";
+import CardSearch from "../common/CardSearch";
 import scream from "../../assets/scream.jpg";
 import hunger_games from "../../assets/poster_movie.jpg";
 import { MdOutlineVerified } from "react-icons/md";
 import home_alone from "../../assets/home_alone.jpg";
+import Modal from "react-bootstrap/Modal";
+import {useState} from "react";
 
 const movieData = [
   {
@@ -119,6 +121,11 @@ const addImg = {
 
 const ProfileSettings = () => {
 
+const [show, setShow] = useState(false);
+
+const handleClose = () => setShow(false); 
+const handleShow = () => setShow(true);
+
   return <>
       <Container className="px-1" fluid style={backgroundContainer}>
         <Navigation />
@@ -182,12 +189,13 @@ const ProfileSettings = () => {
         </Row>
       </Container>
 
+
       <Container fluid style={secondContainer}>
         <Row className="p-2 justify-content-center">
-          <h1 className="mt-4 px-4">Recently viewed</h1>
+          <h1 className="mt-4 px-4">Recently searched</h1>
         {movieData.map((movie) => (
-          <Col key={movie.id} sm={6} md={4} lg={2} className="mb-4 mx-2">
-            <CardComp
+          <Col key={movie.id} sm={6} md={4} lg={2} className="mb-5 mx-2">
+            <CardSearch
               title={movie.title}
               text={movie.text}
               btnText={movie.btnText}
@@ -195,6 +203,28 @@ const ProfileSettings = () => {
             />
           </Col>
         ))}
+        </Row >
+
+        <Row className="py-3">
+          <Col className="my-4 text-center"><Button variant="danger" style={{fontSize:"23px", fontWeight: "bold", padding: "1rem"}} onClick={handleShow}>Delete all search history</Button>
+          <Modal show={show} onHide={handleClose} backdrop="static"
+        keyboard={false} style={{
+          fontSize: "20px",
+          fontWeight: "bold",
+        }} >
+        <Modal.Header closeButton>
+          <Modal.Title>Delete search</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete the whole history? You cannot undo this action.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose} style={{fontWeight: "bold", fontSize: "20px"}}>
+            Yes
+          </Button>
+          <Button variant="warning" onClick={handleClose} style={{fontWeight: "bold", fontSize: "20px"}}>
+            No, take me back!
+          </Button>
+        </Modal.Footer>
+      </Modal></Col>
         </Row>
       </Container>
   </>
