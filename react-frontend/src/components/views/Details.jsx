412a09473bbd0ba2ebf19be0e2b848c393934332
cristinaBodnari <cristina.bodnari12@gmail.com>
2023-12-13 10:14:18 +0100

    Details

--- react-frontend/src/components/views/Details.jsx
+++ react-frontend/src/components/views/Details.jsx
@@ -1,46 +1,28 @@
-import departed from "../../assets/departed.jpg";
+import CustomContainer from "../common/CustomContainer";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CardComp from "../common/CardComp";
+import Dropdowns from "../common/Dropdown";
 
 const Details = () => {
-
-return (
-  <section className="bg-white dark:bg-gray-900 m-6 p-4">
-    <div className="container flex flex-col items-center px-4 py-12 mx-auto xl:flex-row">
-      <div className="flex justify-center xl:w-1/2">
-        <img
-          className="h-80 w-80 sm:w-[28rem] sm:h-[28rem] flex-shrink-0 object-cover rounded-md"
-          src={departed}
-          // alt={type}
-        />
+  return (
+    <CustomContainer fluid>
+      <div style={{ maxWidth: "85%", margin: "0 auto" }}>
+        <Row className="justify-content-between align-items-start">
+          <Row
+            style={{ marginLeft: "60px" }}
+            xs={12}
+            md={3}
+            lg={3}
+            className="mb-4 d-none d-md-block"
+          >
+            <Dropdowns className="ml-4" />
+          </Row>
+          {}
+        </Row>
       </div>
-
-      <div className="flex flex-col items-center mt-6 xl:items-start xl:w-1/2 xl:mt-0">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-800 xl:text-4xl dark:text-white">
-          {"Type"}
-        </h2>
-
-        <p className="block max-w-2xl mt-4 text-xl text-gray-500 dark:text-gray-300">
-          Start/End year: {2015} - {2020}
-        </p>
-
-        <p className="block max-w-2xl mt-4 text-xl text-gray-500 dark:text-gray-300">
-        Type: {"Type"}
-        </p>
-
-        <p className="block max-w-2xl mt-4 text-xl text-gray-500 dark:text-gray-300">
-        Rating: {1}
-        </p>
-        <span className="m-2 p-2 bg-slate-300 text-slate-800 rounded-md">
-          {"description"}
-        </span>
-
-        <button>See similar Movies</button>
-
-        <button>Bookmark</button>
-      <button>Rate</button>
-      </div>
-    </div>
-  </section>
-);
+    </CustomContainer>
+  );
 };
 export default Details;
 
