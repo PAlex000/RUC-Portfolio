7d125960f65b87a7000f0c578b0bfd36701f998d
cristinaBodnari <cristina.bodnari12@gmail.com>
2023-12-13 10:14:18 +0100

    Details

--- react-frontend/src/components/views/Details.jsx
+++ react-frontend/src/components/views/Details.jsx
@@ -1,8 +1,27 @@
+import CustomContainer from "../common/CustomContainer";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CardComp from "../common/CardComp";
+import Dropdowns from "../common/Dropdown";
+
 const Details = () => {
   return (
-    <div>
-      <p>sup sup sup</p>
-    </div>
+    <CustomContainer fluid>
+      <div style={{ maxWidth: "85%", margin: "0 auto" }}>
+        <Row className="justify-content-between align-items-start">
+          <Row
+            style={{ marginLeft: "60px" }}
+            xs={12}
+            md={3}
+            lg={3}
+            className="mb-4 d-none d-md-block"
+          >
+            <Dropdowns className="ml-4" />
+          </Row>
+          {}
+        </Row>
+      </div>
+    </CustomContainer>
   );
 };
 
