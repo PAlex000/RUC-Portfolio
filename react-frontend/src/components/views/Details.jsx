fe74c17947c32009195c3533d473f28d611d2b7d
cristinaBodnari <cristina.bodnari12@gmail.com>
2023-12-13 10:14:18 +0100

    Details

--- react-frontend/src/components/views/Details.jsx
+++ react-frontend/src/components/views/Details.jsx
@@ -45,65 +45,22 @@ const Details = () => {
   };
 
   return (
-    <div>
-      <section className="bg-white dark:bg-gray-900 m-6 p-4">
-        <div className="container flex flex-col items-center px-4 py-12 mx-auto xl:flex-row">
-          <div className="flex justify-center xl:w-1/2">
-            <img
-              className="h-80 w-80 sm:w-[28rem] sm:h-[28rem] flex-shrink-0 object-cover rounded-md"
-              src={poster}
-            />
-          </div>
-
-          <div className="flex flex-col items-center mt-6 xl:items-start xl:w-1/2 xl:mt-0">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-800 xl:text-4xl dark:text-white">
-              {type}
-            </h2>
-
-            <p className="block max-w-2xl mt-4 text-xl text-gray-500 dark:text-gray-300">
-              Start/End year: {startYear} - {endYear}
-            </p>
-
-            <p className="block max-w-2xl mt-4 text-xl text-gray-500 dark:text-gray-300">
-              Type: {type}
-            </p>
-
-            <p className="block max-w-2xl mt-4 text-xl text-gray-500 dark:text-gray-300">
-              Rating: {rating}
-            </p>
-
-            <span className="m-2 p-2 bg-slate-300 text-slate-800 rounded-md">
-              {description}
-            </span>
-
-            <button onClick={handleSeeSimilarMovies}>See similar Movies</button>
-
-            <button>Bookmark</button>
-            <button>Rate</button>
-          </div>
-        </div>
-      </section>
-
-      {similarMoviesVisible && (
-        <div>
-          <h2>List of Similar Movies</h2>
-          <div style={movieGridStyle}>
-            {movies.$values.map((movie) => (
-              <div key={movie.id} style={movieItemStyle}>
-                <img src={movie.poster} alt={`Poster for ${movie.id}`} style={moviePosterStyle} />
-                <div className="movie-details">
-                  <h3 className="movie-title">
-                    {movie.akas && movie.akas.$values && movie.akas.$values[0] && movie.akas.$values[0].title}
-                  </h3>
-                  <p className="movie-description">{movie.description}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-      
-    </div>
+    <CustomContainer fluid>
+      <div style={{ maxWidth: "85%", margin: "0 auto" }}>
+        <Row className="justify-content-between align-items-start">
+          <Row
+            style={{ marginLeft: "60px" }}
+            xs={12}
+            md={3}
+            lg={3}
+            className="mb-4 d-none d-md-block"
+          >
+            <Dropdowns className="ml-4" />
+          </Row>
+          {}
+        </Row>
+      </div>
+    </CustomContainer>
   );
 };
 
