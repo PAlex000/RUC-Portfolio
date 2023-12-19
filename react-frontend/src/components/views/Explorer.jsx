cd2b21b31252109b57990fb38ef27c2f3c7b6f24
Pozsgai Alex <jatekalex@gmail.com>
2023-12-19 02:13:11 +0100

    Watchlist Fix

--- react-frontend/src/components/views/Explorer.jsx
+++ react-frontend/src/components/views/Explorer.jsx
@@ -102,7 +102,7 @@ const Explorer = () => {
                   btnText="Learn More"
                   image={movie.poster}
                   rating={movie.rating}
-                  dispatch={dispatch}
+                  dispatchMovie={dispatch}
                 />
               </Col>
             ))
