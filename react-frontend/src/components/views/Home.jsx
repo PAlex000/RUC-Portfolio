ae64be044b76351e643dcb02dbcf68f97e01244b
Pozsgai Alex <jatekalex@gmail.com>
2023-12-19 02:16:16 +0100

    Remove headerData

--- react-frontend/src/components/views/Home.jsx
+++ react-frontend/src/components/views/Home.jsx
@@ -5,26 +5,7 @@ import Row from "react-bootstrap/Row";
 import { Button } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import CardComp from "../common/CardComp";
-import Header from "../layout/Header";
-import header from "../../assets/movieHeader.jpg";
 import CustomContainer from "../common/CustomContainer";
-
-const headerData = [
-  {
-    imageUrl: header,
-    overlayText: {
-      title: "First Image Title",
-      description: "Description for the first image",
-    },
-  },
-  {
-    imageUrl: header,
-    overlayText: {
-      title: "Second Image Title",
-      description: "Description for the second image",
-    },
-  },
-];
 
 const marginStyle = {
   marginTop: "14.9px",
@@ -51,7 +32,6 @@ const Home = () => {
   if (!movies) return <div>No movies available</div>;
   return (
     <CustomContainer fluid>
-      <Header headers={headerData} />
       <div style={{ maxWidth: "75%", margin: "0 auto" }}>
         <Row className="justify-content-between align-items-center">
           <Row xs={12} md={3} lg={2} className="mb-4 d-none d-md-block">
